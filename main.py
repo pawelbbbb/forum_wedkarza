0e3f0844517aa69ba2aead405b0e214be0b83c36
Paweł <147084723+pawelbbbb@users.noreply.github.com>
2025-11-22 19:27:51 +0100

    Modify db_pass retrieval to use specific key
    Updated db_pass to retrieve password from a specific key in the environment variable.

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from datetime import date
 import bcrypt
 import os
 
-db_pass = os.getenv('DB_PASS')
+db_pass = os.getenv('DB_PASS')["forum-db-password"]
 db_user = os.getenv('DB_USER')
 db_host = os.getenv('DB_HOST')
-
-print("DB_PASS:", db_pass)
-print("DB_USER:", db_user)
-print("DB_HOST:", db_host)
 
 print("credentials received")
 con = mysql.connector.connect(user=db_user, password=db_pass, host=db_host, database='forum')
